cd18fb74e674c594bd40df24389f3264a580c946
fernando.nardi <fernando.nardi@senior.com.br>
2023-02-17 15:35:50 -0300

    Adicionado - Custom Exception Handling

--- src/main/java/com/supportportal/resource/UserResource.java
+++ src/main/java/com/supportportal/resource/UserResource.java
@@ -1,15 +1,17 @@
 package com.supportportal.resource;
 
+import com.supportportal.exception.domain.ExceptionHandling;
+import com.supportportal.exception.domain.UserNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
-public class UserResource {
+@RequestMapping(path = {"/", "/user"})
+public class UserResource extends ExceptionHandling {
 
-    @GetMapping("/user")
-    public String showUSer() {
-        return "application works";
+    @GetMapping("/home")
+    public String showUSer() throws UserNotFoundException { //
+        throw new UserNotFoundException("The user is not found.");
     }
 }
