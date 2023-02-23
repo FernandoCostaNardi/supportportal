b02bc3f19b68bcfb8c887ce8df7fc0050cacc94c
fernando.nardi <fernando.nardi@senior.com.br>
2023-02-23 18:06:18 -0300

    Generate JWT

--- src/main/java/com/supportportal/resource/UserResource.java
+++ src/main/java/com/supportportal/resource/UserResource.java
@@ -1,17 +1,23 @@
 package com.supportportal.resource;
 
+import com.supportportal.domain.UserPrincipal;
 import com.supportportal.domain.Users;
 import com.supportportal.exception.domain.EmailExistException;
 import com.supportportal.exception.domain.ExceptionHandling;
 import com.supportportal.exception.domain.UsernameExistException;
 import com.supportportal.service.UserService;
+import com.supportportal.utility.JWTTokenProvider;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.supportportal.constant.SecurityConstant.JWT_TOKEN_HEADER;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -20,10 +26,31 @@ import static org.springframework.http.HttpStatus.OK;
 public class UserResource extends ExceptionHandling {
 
     private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private JWTTokenProvider tokenProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<Users> login(@RequestBody Users user) throws EmailExistException, UsernameExistException { //
+       authenticate(user.getUsername(), user.getPassword());
+       Users loginUser = userService.findUserByUsername(user.getUsername());
+       UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+       HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+       return new ResponseEntity<>(loginUser, jwtHeader, OK);
+    }
 
     @PostMapping("/register")
     public ResponseEntity<Users> register(@RequestBody Users user) throws EmailExistException, UsernameExistException { //
         Users newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
         return new ResponseEntity<>(newUser, OK);
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal userPrincipal) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JWT_TOKEN_HEADER, tokenProvider.generateJwtToken(userPrincipal));
+        return headers;
+    }
+
+    private void authenticate(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
     }
 }
