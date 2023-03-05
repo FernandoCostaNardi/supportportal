58b429f9c42af1dec838a9e31000c62694812d59
fernando.nardi <fernando.nardi@senior.com.br>
2023-03-05 18:15:49 -0300

    User Resource

--- src/main/java/com/supportportal/resource/UserResource.java
+++ src/main/java/com/supportportal/resource/UserResource.java
@@ -1,38 +1,52 @@
 package com.supportportal.resource;
 
+import com.supportportal.domain.HttpResponse;
 import com.supportportal.domain.UserPrincipal;
 import com.supportportal.domain.Users;
 import com.supportportal.exception.domain.EmailExistException;
+import com.supportportal.exception.domain.EmailNotFoundException;
 import com.supportportal.exception.domain.ExceptionHandling;
 import com.supportportal.exception.domain.UsernameExistException;
 import com.supportportal.service.UserService;
 import com.supportportal.utility.JWTTokenProvider;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+import static com.supportportal.constant.FileConstant.*;
 import static com.supportportal.constant.SecurityConstant.JWT_TOKEN_HEADER;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping(path = {"/", "/user"})
 public class UserResource extends ExceptionHandling {
 
+    public static final String EMAIL_SENT = "An email with a new password was sent to: ";
+    public static final String USER_DELETED_SUCCESSFULLY = "User deleted successfully";
     private UserService userService;
     private AuthenticationManager authenticationManager;
     private JWTTokenProvider tokenProvider;
 
     @PostMapping("/login")
-    public ResponseEntity<Users> login(@RequestBody Users user) throws EmailExistException, UsernameExistException { //
+    public ResponseEntity<Users> login(@RequestBody Users user) { //
        authenticate(user.getUsername(), user.getPassword());
        Users loginUser = userService.findUserByUsername(user.getUsername());
        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
@@ -44,6 +58,92 @@ public class UserResource extends ExceptionHandling {
     public ResponseEntity<Users> register(@RequestBody Users user) throws EmailExistException, UsernameExistException, MessagingException { //
         Users newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
         return new ResponseEntity<>(newUser, OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Users> addNewUser(@RequestParam("firstName") String firstName,
+                                            @RequestParam("lastName") String lastName,
+                                            @RequestParam("username") String username,
+                                            @RequestParam("email") String email,
+                                            @RequestParam("role") String role,
+                                            @RequestParam("isActive") String isActive,
+                                            @RequestParam("isNonLocked") String isNonLocked,
+                                            @RequestParam(value = "profileImage") MultipartFile profileImage) throws EmailExistException, IOException, UsernameExistException {
+        Users newUSer = userService.addNewUser(firstName, lastName, username,
+                email, role, Boolean.parseBoolean(isActive), Boolean.parseBoolean(isNonLocked), profileImage);
+        return new ResponseEntity<>(newUSer, OK);
+
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Users> update(@RequestParam("currentUserName") String currentUserName,
+                                            @RequestParam("firstName") String firstName,
+                                            @RequestParam("lastName") String lastName,
+                                            @RequestParam("username") String username,
+                                            @RequestParam("email") String email,
+                                            @RequestParam("role") String role,
+                                            @RequestParam("isActive") String isActive,
+                                            @RequestParam("isNonLocked") String isNonLocked,
+                                            @RequestParam(value = "profileImage") MultipartFile profileImage) throws EmailExistException, IOException, UsernameExistException {
+        Users updatedUSer = userService.updateUser(currentUserName, firstName, lastName, username,
+                email, role, Boolean.parseBoolean(isActive), Boolean.parseBoolean(isNonLocked), profileImage);
+        return new ResponseEntity<>(updatedUSer, OK);
+
+    }
+
+    @GetMapping("/find/{username}")
+    public ResponseEntity<Users> getUser(@PathVariable("username") String username) {
+        Users user = userService.findUserByUsername(username);
+        return new ResponseEntity<>(user, OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Users>> getAllUser() {
+        List<Users> users = userService.getUsers();
+        return new ResponseEntity<>(users, OK);
+    }
+
+    @GetMapping("/resetPassword/{email}")
+    public ResponseEntity<HttpResponse> resetPassword(@PathVariable("email") String email) throws EmailNotFoundException, MessagingException {
+       userService.resetPassword(email);
+        return response(OK, EMAIL_SENT + email);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('user:delete')")
+    public ResponseEntity<HttpResponse> deleteUser(@PathVariable("id") long id) {
+        userService.deleteUser(id);
+        return response(NO_CONTENT, USER_DELETED_SUCCESSFULLY);
+    }
+
+    @PostMapping("/updateProfileImage")
+    public ResponseEntity<Users> updateProfileImage(@RequestParam("username") String username,
+                                                   @RequestParam(value = "profileImage") MultipartFile profileImage) throws EmailExistException, IOException, UsernameExistException {
+        Users user = userService.updateProfileImage(username, profileImage);
+        return new ResponseEntity<>(user, OK);
+    }
+
+    @GetMapping(value = "/image/{profile}/{username}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getTempProfileImage(@PathVariable("username") String username) throws IOException {
+        URL url = new URL(TEMP_PROFILE_IMAGE_BASE_URL + username);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (InputStream inputStream = url.openStream()) {
+            int bytesRead;
+            byte[] chunk = new byte[1024];
+            while((bytesRead = inputStream.read(chunk)) > 0) {
+                byteArrayOutputStream.write(chunk, 0, bytesRead);
+            }
+        }
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    @GetMapping(value = "/image/{username}/{fileName}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getProfileImage(@PathVariable("username") String username, @PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(USER_FOLDER + username + FORWARD_SLASH + fileName));
+    }
+
+    private ResponseEntity<HttpResponse> response(HttpStatus httpStatus, String message) {
+        return new ResponseEntity<>(new HttpResponse(httpStatus.value(), httpStatus, httpStatus.getReasonPhrase(),message.toUpperCase()),httpStatus);
     }
 
     private HttpHeaders getJwtHeader(UserPrincipal userPrincipal) {
